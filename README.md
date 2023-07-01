82e64fe1bf80c746e8378b8b6b35e8c720d5756b
Muhammad Faraz Shahid <farazshahid123456@gmail.com>
2023-07-01 15:57:46 +0100

    portfolio

---
+++ README.md
@@ -0,0 +1,6 @@
+# Portfolio website created using React.js - Three.js - TailwindCSS
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/abc944d5-ad2a-4fb8-a2b1-398a311dbae9/deploy-status)](https://app.netlify.com/sites/enchanting-chaja-68169a/deploys)
+[![](https://img.shields.io/badge/Live%20Website%20Link%20~%20-Krishnasahu.in-brightgreen&style=flat)](https://krishnasahu.in/)
+
+![githubusercontent](https://repository-images.githubusercontent.com/613266429/d06b3c4f-d844-4cda-803f-5c40947c7cc0) 
---
+++ src/assets/index.js
@@ -0,0 +1,89 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import lionbridge from "./company/lionbridge.png";
+import shopify from "./company/shopify.png";
+import accenture from "./company/accenture.png";
+import viztar from "./company/viztar.png";
+import webstream from "./company/webstream.png";
+import tesla from "./company/tesla.png";
+import ashish from "./company/ashish.jpg";
+import zaid from "./company/zaid.jpg";
+
+import carrent from "./carrent.png";
+import schwabe from "./schwabe.png";
+import airportcodes from "./airportcodes.png";
+import jobit from "./jobit.png";
+import portfolio from "./portfolio.png";
+import tripguide from "./tripguide.png";
+import sharktank from "./sharktank.png";
+import siddesh from "./siddesh.png";
+
+import linkedin from "./linkedin.png";
+import twitter from "./twitter.png";
+import instagram from "./instagram.png";
+import rupee from "./rupee.png";
+
+export {
+  linkedin,
+  twitter,
+  instagram,
+  rupee,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  ashish,
+  zaid,
+  siddesh,
+  sharktank,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  lionbridge,
+  shopify,
+  accenture,
+  webstream,
+  viztar,
+  tesla,
+  carrent,
+  airportcodes,
+  portfolio,
+  schwabe,
+  jobit,
+  tripguide,
+};
